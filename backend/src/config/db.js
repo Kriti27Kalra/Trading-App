095a4d12d572ffb7e0307be1f0349d354649dec9
Kriti27Kalra <kritikalra2002@gmail.com>
2025-06-06 18:50:56 +0530

    db.js updated

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // Load environment variables from .env file
 
 // Create the database connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,  // Database host (from .env)
+  host: '127.0.0.1',  // Database host (from .env)
   user: process.env.DB_USER,  // Database user (from .env)
   password: process.env.DB_PASSWORD,  // Database password (from .env)
   database: process.env.DB_NAME,  // Database name (from .env)
